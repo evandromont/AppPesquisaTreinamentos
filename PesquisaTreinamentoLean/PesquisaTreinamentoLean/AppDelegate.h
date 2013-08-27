c9503a4080681969acc9765e7e0b8e42a53459e5
evandromont <evandro.montenegro23@gmail.com>
2013-08-27 16:34:18 -0300

    Design do APP
    Montei as telas, coloquei as imagens e bootes.

---
+++ PesquisaTreinamentoLean/PesquisaTreinamentoLean/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PesquisaTreinamentoLean
+//
+//  Created by Evandro Montenegro on 27/08/13.
+//  Copyright (c) 2013 iTmSense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PesquisaTreinamentoLean/PesquisaTreinamentoLean/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PesquisaTreinamentoLean
+//
+//  Created by Evandro Montenegro on 27/08/13.
+//  Copyright (c) 2013 iTmSense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
